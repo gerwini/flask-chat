29def9207e3ebdbd9c315044a98a9ca13e5aee42
gerwini <gerwinjb@hotmail.nl>
2020-10-12 20:48:05 +0000

    too many blank lines, message not used

--- app.py
+++ app.py
@@ -10,12 +10,8 @@ messages = []
 def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}){}: {}".format(now, username, message))
-
-
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,10 +27,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session['username'])
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
 @app.route("/<username>/<message>")
